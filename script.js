2b8982edd00f5ca832e99e8ee8cfe6d918e9103b
Rushmerk <57403682+pedroperegrinaa@users.noreply.github.com>
2021-11-29 01:14:14 -0300

    modulo 03
    aula 22

---
+++ script.js
@@ -0,0 +1,29 @@
+const paragrafos = document.querySelector('.paragrafos')
+const ps = document.querySelectorAll('p')
+
+const estilosBody = getComputedStyle(document.body);
+
+let pegaCor;
+
+console.log(estilosBody)
+
+// console.log(ps)
+// console.log(paragrafos)
+
+
+
+for (let c in estilosBody) {
+    if (c == 'backgroundColor') {
+        console.log(c, estilosBody[c])
+        pegaCor = estilosBody[c];
+
+    }
+}
+
+// pegaCor = estilosBody.backgroundColor
+console.log(pegaCor)
+
+for (let p of ps) {
+    p.style.backgroundColor = pegaCor
+    p.style.color = '#FFFFFF'
+}
